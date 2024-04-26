42e1ffd857743592b849bcf87aec760d3863ab48
Valter Gabriel <valtergabrielbs13@gmail.com>
2024-04-26 13:29:49 -0300

    ms java
    ms java

---
+++ ms-java/src/main/java/msjava/processing/transferencia/ProcessingTransferenciaApplication.java
@@ -0,0 +1,13 @@
+package msjava.processing.transferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessingTransferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessingTransferenciaApplication.class, args);
+	}
+
+}
---
+++ ms-java/src/test/java/msjava/processing/transferencia/ProcessingTransferenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package msjava.processing.transferencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessingTransferenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
